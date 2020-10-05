146177d5623930d8b902dea71a7d392119bbab32
Manav Misra <manavm@visionwebsoft.com>
2020-10-05 17:21:40 -0500

    :construction: Query selector all relevant items

---
+++ src/scripts.js
@@ -0,0 +1,10 @@
+const root = document.documentElement;
+
+const button = document.querySelector("button");
+const display = document.querySelector("#display");
+const colorInput = document.querySelector("[type='color']");
+const palettes = document.querySelectorAll("#palette div");
+
+const tools = document.querySelectorAll("[type='range']");
+
+console.log(tools);
